14b79b3ee9a3e6497d616ce4a636dd1c126207a4
phillip nzau junior <nzau78@gmail.com>
2021-08-31 10:15:14 +0300

    more tab set up

--- football-league/src/app/results/results-routing.module.ts
+++ football-league/src/app/results/results-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ResultsComponent } from './results.component';
 
-const routes: Routes = [{ path: '', component: ResultsComponent }, { path: 'results-list', loadChildren: () => import('./results-list/results-list.module').then(m => m.ResultsListModule) }, { path: 'results-view', loadChildren: () => import('./results-view/results-view.module').then(m => m.ResultsViewModule) }, { path: 'results-add', loadChildren: () => import('./results-add/results-add.module').then(m => m.ResultsAddModule) }];
+const routes: Routes = [{ path: '', component: ResultsComponent },
+  { path: 'results-list', loadChildren: () => import('./results-list/results-list.module').then(m => m.ResultsListModule) },
+  { path: 'results-view', loadChildren: () => import('./results-view/results-view.module').then(m => m.ResultsViewModule) },
+  { path: 'results-add', loadChildren: () => import('./results-add/results-add.module').then(m => m.ResultsAddModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
